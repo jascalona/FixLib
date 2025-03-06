996804d923d8854298d47d4e6c5c73e5271dfdc8
Jose Escalona <JEscalona@lan.xven.com.ve>
2025-03-06 09:38:18 -0400

    se creo repositorio para el desarrollo de FixLib APP para Windows, ya se desarrollo la interfaz de Login y actualmente estoy trabajando en la interfaz LayerLib donde se alojan los modulos de los equipos , NOTA: DEBES TRABAJAR LAS VISTAS CANVAS

---
+++ src/main/java/grupoxdv/com/servicelibrary/ServiceLibrary.java
@@ -0,0 +1,16 @@
+package grupoxdv.com.servicelibrary;
+import grupoxdv.com.servicelibrary.layerLog;
+
+
+/**
+ *
+ * @author JEscalona
+ */
+public class ServiceLibrary {
+
+    public static void main(String[] args) {
+        layerLog layerL = new layerLog();
+        layerL.setVisible(true);
+        
+    }
+}
