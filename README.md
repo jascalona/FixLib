26638b1c3a7de3261cd4eb60fe65156a850a7dc4
Jose Escalona <JEscalona@lan.xven.com.ve>
2025-03-11 16:45:45 -0400

    se generaron cambios en el LayerDoc

---
+++ README.md
@@ -0,0 +1,2 @@
+# FixLib Es una App desarrollada con el fin de optimizar la gestion de su contenido: los usuarios tienen control total sobre como se utiliza su contenido. los usuarios pueden anotar, resaltar, filtrar e interactuar con su contenido ahora en despositivos Windows.
+# FixLib
---
+++ src/fixlib/FixLib.java
@@ -0,0 +1,18 @@
+package fixlib;
+import fixlib.LayerLog;
+
+/**
+ *
+ * @author JEscalona
+ */
+public class FixLib {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LayerLog LayerL = new LayerLog();
+        LayerL.setVisible(true);
+    }
+    
+}
